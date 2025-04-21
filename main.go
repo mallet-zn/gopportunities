9e365824a62276ca0505fa0b1dc30abfb33d688d
Matheus Mallet <matheusphlippe.mallet@gmail.com>
2025-04-20 22:35:08 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
-import "github.com/mallet-zn/gopportunities/router"
+import (
+	"github.com/mallet-zn/gopportunities/config"
+	"github.com/mallet-zn/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Config Initialize
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config Initialization error: %v", err)
+		return
+	}
 
 	// Router Initialize
 	router.Initialize()
-
 }
